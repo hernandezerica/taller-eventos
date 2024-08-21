d68dc35e3b7337b59789633d38a6c29a03eb6aa2
hernandezerica <ericahernandez201717@gmail.com>
2024-08-21 19:17:19 -0300

    Sitio web creado

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var div = document.getElementById("btnContainer")
+    div.onclick = function saludoDiv() { alert("Hola! Soy el div") }
+});
